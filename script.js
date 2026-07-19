808f69dfa724f77103c7f61f8d2033d143bf3c70
Marcio-itu <marcio_itu@hotmail.com>
2026-07-18 23:52:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Cartão Digital Interativo carregado com sucesso.");
-
     const hotspots = document.querySelectorAll('.hotspot');
-    
     hotspots.forEach(button => {
         button.addEventListener('click', (e) => {
-            // Executa vibração curta no celular se houver suporte
-            if ('vibrate' in navigator) {
-                navigator.vibrate(60);
-            }
+            if ('vibrate' in navigator) navigator.vibrate(60);
         });
     });
 });

652d88fb1188e6ed1bf9a57c60de05f5fe0329bc
Marcio-itu <marcio_itu@hotmail.com>
2026-07-23 11:05:04 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hotspots = document.querySelectorAll('.hotspot');
-    hotspots.forEach(button => {
-        button.addEventListener('click', (e) => {
-            if ('vibrate' in navigator) navigator.vibrate(60);
-        });
-    });
-});
